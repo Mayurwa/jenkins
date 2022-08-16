dc5a446eb998dd80b9f458237b8cbeedcfeead63
Mayurwa <waghmare.p.mayur@gmail.com>
2022-08-16 14:40:53 +0530

    first commit

---
+++ src/CollectionDemo.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CollectionDemo {
+
+	public static void main(String args[])
+	{
+		List<String> list= new ArrayList();
+		list.add("Mayur");
+		list.add("Nikhil");
+		list.add("Pratu");
+		
+		for(String a:list)
+		{
+			System.out.println(a);
+		}
+		
+		LinkedList<String> link= new LinkedList<>();
+	
+	}
+}
